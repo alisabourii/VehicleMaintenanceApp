ff1ccad2d09e75566e3c35b9c13c15a48dda9e5e
Ali <72344723+alisabourii@users.noreply.github.com>
2025-08-12 11:46:54 +0300

    Make spinner protitip test

--- app/src/main/java/com/example/vehiclemaintenanceapp/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/vehiclemaintenanceapp/ui/slideshow/SlideshowFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -13,9 +14,32 @@ class SlideshowFragment : Fragment() {
 
     private var _binding: FragmentSlideshowBinding? = null
 
+    private lateinit var typeSpinner: Spinner
+    private lateinit var brandSpinner: Spinner
+    private lateinit var modelSpinner: Spinner
+    private lateinit var yearSpinner: Spinner
+
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val carData = mapOf(
+        "Otomobil" to mapOf(
+            "BMW" to mapOf(
+                "3 Series" to listOf("2020", "2021", "2022"),
+                "X5" to listOf("2018", "2019", "2020")
+            ),
+            "Toyota" to mapOf(
+                "Corolla" to listOf("2017", "2018", "2019", "2020")
+            )
+        ),
+        "SUV" to mapOf(
+            "Jeep" to mapOf(
+                "Cherokee" to listOf("2015", "2016", "2017")
+            )
+        )
+    )
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,10 +52,7 @@ class SlideshowFragment : Fragment() {
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
